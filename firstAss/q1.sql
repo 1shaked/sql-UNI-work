75b3ad589b1b10b128b78aba15732d9c69891ac7
1shaked <shaked1hen@gmail.com>
2022-12-14 10:33:36 +0200

    comment line q1

--- firstAss/q1.sql
+++ firstAss/q1.sql
@@ -1,13 +1,12 @@
 # assume we needed to lower the text so word like Aa can not appear in the category name
 SELECT 
-	c.category_id
+	# c.category_id # if you want to show the category id just uncomment the line
     ,c.name
-    # ,c.last_update
 FROM sakila.category as c
 WHERE 
 	# lower the name col and check if the string contain a only one time
 	LOWER(NAME) REGEXP '^[^a]*a[^a]*$'
-    
+    # another option to implement the same function just comment line 8 and uncomment line 10 
     # LOWER(NAME) like '%a%' AND LOWER(NAME) NOT LIKE '%a%a%'
 ORDER BY name
 ;
